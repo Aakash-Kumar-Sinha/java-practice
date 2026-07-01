0addc26235b7726b2fe181fb1b47029fb3db74fa
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-01 20:42:08 +0530

    Initial commit

---
+++ CompanyDetails/src/org/company/CompanyInfo.java
@@ -0,0 +1,25 @@
+package org.company;
+
+public class CompanyInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CompanyInfo cmp = new CompanyInfo();
+		cmp.companyName();
+		cmp.companyId();
+		cmp.companyAddress();
+	}
+	
+	public void companyName() {
+		System.out.println("Company Name");
+	}
+	
+	public void companyId() {
+		System.out.println("Company Id");
+	}
+	
+	public void companyAddress() {
+		System.out.println("Company Address");
+	}
+
+}
