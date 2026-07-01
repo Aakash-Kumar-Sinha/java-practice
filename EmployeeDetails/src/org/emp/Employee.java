63e47bbbbaf205377cc5da004298c60add512ecd
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-01 20:40:10 +0530

    Initial commit

---
+++ EmployeeDetails/src/org/emp/Employee.java
@@ -0,0 +1,40 @@
+package org.emp;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp = new Employee();
+		emp.empId();
+		emp.empName();
+		emp.empDob();
+		emp.empPhone();
+		emp.empEmail();
+		emp.empAddress();
+	}
+	
+	public void empId() {
+		System.out.println("Employee Id");
+	}
+	
+	public void empName() {
+		System.out.println("Employee Name");
+	}
+	
+	public void empDob() {
+		System.out.println("Employee DOB");
+	}
+	
+	public void empPhone() {
+		System.out.println("Employee Phone");
+	}
+	
+	public void empEmail() {
+		System.out.println("Employee Email");
+	}
+	
+	public void empAddress() {
+		System.out.println("Employee Address");
+	}
+
+}
