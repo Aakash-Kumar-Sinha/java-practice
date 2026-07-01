70167315b598b1bc8be0506022e601476bf99b27
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-01 20:41:42 +0530

    Initial commit

---
+++ BesandAddress/src/org/add/BesandTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class BesandTech {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BesandTech bt = new BesandTech();
+		bt.BesandOmr();
+		bt.BesandAdyar();
+		bt.BesandTambaram();
+		bt.BesandVelacherry();
+		bt.BesandAnnaNagar();
+	}
+	
+	public void BesandOmr() {
+		System.out.println("BesandOmr");
+	}
+	
+	public void BesandAdyar() {
+		System.out.println("BesandAdyar");
+	}
+	
+	public void BesandTambaram() {
+		System.out.println("BesandTambaram");
+	}
+	
+	public void BesandVelacherry() {
+		System.out.println("BesandVelacherry");
+	}
+	
+	public void BesandAnnaNagar() {
+		System.out.println("BesandAnnaNagar");
+	}
+
+}
