89dd6c2320f4653e41d8c336d07b9019dc1e5f7b
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-06 16:20:21 +0530

    Initial Commit

---
+++ 03_Operators/AreaOfCircle.java
@@ -0,0 +1,23 @@
+/* #1:
+Description:
+Write a program to calculate the Area of a Circle.
+Use double for radius and pi (3.14).
+area = pi * radius * radius;
+Example:
+Input Radius = 5
+Output = 78.5 */
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        int radius = sc.nextInt();
+
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle is: " + area);
+    }
+}
