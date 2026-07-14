93f31b6bbfc945af55372e79671fd816eadf5f1a
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-14 17:12:59 +0530

    Initial Commit

---
+++ 05_Control Statements/Patterns.java
@@ -0,0 +1,39 @@
+public class Patterns {
+    public static void main(String[] args) {
+
+        // Pattern 1
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Pattern 2
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Pattern 3
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 3; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
