dcfe1bf98133d5785a3a9b6896db7377f84bc00d
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-14 17:15:41 +0530

    Initial Commit

---
+++ 05_Control Statements/CountNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while (num > 0) {
+            num = num / 10;
+            count++;
+        }
+
+        System.out.println("Count of number = " + count);
+
+        sc.close();
+    }
+}
+
+
+// Output:
+// Enter a number: 123
+// Count of number = 3
