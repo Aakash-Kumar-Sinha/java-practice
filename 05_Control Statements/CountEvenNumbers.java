07cab67c63f313cd6fc0283ba89f200cda95e4ac
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-14 17:07:21 +0530

    Initial Commit

---
+++ 05_Control Statements/CountEvenNumbers.java
@@ -0,0 +1,18 @@
+public class CountEvenNumbers {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Count of even numbers = " + count);
+    }
+}
+
+
+// Output:
+// Count of even numbers = 50
